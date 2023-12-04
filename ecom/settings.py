42f3fc7d767b362c41bbc06f02419d89faba1ccd
parashuram kalakutagi <120585101+parashuramkalakutagi@users.noreply.github.com>
2023-12-04 16:35:22 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-eya7@q@^dll%rot_y^&5gh=v-xedjjl(!0_*8w5@^1o-2t-v!2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 DOMAIN = ''
 
